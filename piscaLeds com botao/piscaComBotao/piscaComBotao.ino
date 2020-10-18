70ef4786713fa266c2806360cfec1ffe683b26c8
hermesmc <hermesmc@gmail.com>
2020-10-18 16:49:02 -0300

    Piscando leds com botao

---
+++ piscaLeds com botao/piscaComBotao/piscaComBotao.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int estadoBotao = digitalRead(7);  
+  digitalWrite(9, estadoBotao); 
+  digitalWrite(10, !estadoBotao);  
+}
