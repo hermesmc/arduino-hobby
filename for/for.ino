5b84e4c2f8f134f2264a0b6e12d52893378f2cd1
hermesmc <hermesmc@gmail.com>
2020-10-21 19:25:23 -0300

    lacos com for

---
+++ for/for.ino
@@ -0,0 +1,28 @@
+
+//Declarando array:
+int pinLed[4] = {13,12,11,10};
+int y;
+
+void setup() {
+  // put your setup code here, to run once:
+  int x;
+  for(x = 0; x < 4; x = x + 1){
+     pinMode(pinLed[x], OUTPUT);
+  }  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int y;
+ 
+  for(y=0; y < 4 ; y = y + 1){
+    delay(100); 
+    digitalWrite(pinLed[y], HIGH);    
+    if (y > 0){
+      digitalWrite(pinLed[y - 1], LOW);    
+    }
+    if (y == 0){
+      digitalWrite(pinLed[3], LOW);    
+    }
+  }
+}
