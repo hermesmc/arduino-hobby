edc34a56be4dc36a73ec744813a70ed5e5cc0586
hermesmc <hermesmc@gmail.com>
2020-10-29 08:32:30 -0300

    Ajuste no codigo das portas analogicas

--- portas_analogicas/portas_analogicas.ino
+++ portas_analogicas/portas_analogicas.ino
@@ -1,28 +1,31 @@
-#define pinPIR 2
-int pinLed[9] = {12,11,10,9,8,7,6,5,4};
-int x;
+int pinos[9] = {12,11,10,9,8,7,6,5,4};
+
+float valorLDR;
+float minimo = 85;
+float maximo = 1025;
+float luminosidade;
 
 void setup() {
   // put your setup code here, to run once:
-  for(x = 0; x < 9; x++){
-     pinMode(pinLed[x], OUTPUT);
+  for(int x = 0; x < 9; x++){
+     pinMode(pinos[x], OUTPUT);
   }
-  pinMode(pinPIR, INPUT);  
-  Serial.begin(9600);
+  pinMode(A0, INPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  bool valorPIR = false;
-  valorPIR = digitalRead(pinPIR);
+  valorLDR = analogRead(A0);
 
-  for(x = 0; x < 9; x++){
-    if (valorPIR){  
-      digitalWrite(pinLed[x], HIGH);          
+  luminosidade = ((valorLDR - minimo ) / (maximo - minimo)) * 10;
+  luminosidade = (luminosidade - 10) * -1;
+  for (int y = 0; y < 9; y++){
+    if (y < luminosidade){
+      digitalWrite(pinos[y], HIGH);  
     }else{
-      digitalWrite(pinLed[x], LOW);          
-    }  
-  }    
-  delay(100);  
+       digitalWrite(pinos[y], LOW);   
+    }
+  }  
+  delay(200);  
 }
