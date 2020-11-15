b6a641aa55c6ef3f323f871da24396b8ce512f72
hermesmc <hermesmc@gmail.com>
2020-11-15 09:28:31 -0300

    Labirinto funcional

--- labirinto/labirinto.ino
+++ labirinto/labirinto.ino
@@ -1,33 +1,10 @@
-/* Copie 100% deste documento e cole na área de programação do Arduino IDE */
-/* *********************** Nervo Teste do Manual do Mundo ***********************
-
-   Criado por: Anderson Harayashiki Moreira
-
-   Rev.: 02
-
-   Data: 25.02.2018
-
-   Guia de conexão:
-
-   LED indicativo: Pino 2
-
-   Labirinto: Pino 3
-
-   Botão: Pino 7
-
-   Buzzer: Pino 9
-
-   Módulo Relé (Sirene): Pino 10
-
- ***************************************************************************** */
-
-// Declaração dos pinos utilizados do Arduino
-int pinoLED = 2;
+int pinoLED  = 8;
+int pinoLED1 = 7;
+int pinoLED2 = 6;
+int pinoLED3 = 5;
 int pinoLabirinto = 3;
 int pinoBuzzer = 9;
-int pinoSirene = 10;
 
-// Declaração das variáveis auxiliares
 int nivel = 0;
 long int tempoAnterior = 0;
 int deltaT = 200;
@@ -35,26 +12,21 @@ int tomBuzzer = 3000;
 int estadoBotao = LOW;
 int estadoBuzzer = LOW;
 
-// A função setup é apenas executada uma vez ao se iniciar o Arduino, aqui configuramos nossa placa Arduino
-
 void setup() {
   // Pinos configurados como entrada:
   pinMode(pinoLabirinto, INPUT_PULLUP); // Configurando a entrada como INPUT_PULLUP, utilizamos o resistor interno pull-up do Arduino
 
-  //=======  pinMode(pinoBotao, INPUT);
-  // Pinos configurados como saída:
-
+  nivel = 0;
+  pinMode(pinoLED, OUTPUT);
+  pinMode(pinoLED1, OUTPUT);
+  pinMode(pinoLED2, OUTPUT);
+  pinMode(pinoLED3, OUTPUT);
   pinMode(pinoBuzzer, OUTPUT);
-  pinMode(pinoSirene, OUTPUT);
   digitalWrite(pinoLED, HIGH);
-  digitalWrite(pinoSirene, HIGH); //noTone(pinoSirene);
   Serial.begin(9600);
 }
 
-// A função loop é executada repetitivamente até se desligar o Arduino
-
 void loop() {
-  Serial.println(nivel);
   estadoBotao = digitalRead(pinoLabirinto);
   if (estadoBotao == LOW) {
     delay(50);
@@ -66,29 +38,61 @@ void loop() {
       nivel = nivel + 1;
     }
   }
-  if (nivel == 2) {
+  if (nivel == 1) {
     deltaT = 100;
     tomBuzzer = 3500;
+    digitalWrite(pinoLED1, HIGH);
   }
-  if (nivel == 3) {
+  if (nivel == 2) {
     deltaT = 50;
     tomBuzzer = 4000;
+    digitalWrite(pinoLED2, HIGH);
   }
-  if (nivel == 4) {
-    noTone(pinoBuzzer);//digitalWrite(pinoBuzzer, LOW);
-    while (1) {
-      digitalWrite(pinoSirene, LOW); //tone(pinoSirene, 4000);
-    }
-
+  if (nivel == 3) {
+    deltaT = 0;
+    tomBuzzer = 4000;
+    digitalWrite(pinoLED3, HIGH);
+    estadoBuzzer = HIGH;
   }
+  Serial.println(nivel);
   if (((millis() - tempoAnterior) > deltaT) && (nivel >= 1)) {
     if (estadoBuzzer) {
+      Serial.println("Toca");
       tone(pinoBuzzer, tomBuzzer);
     }
     else {
       noTone(pinoBuzzer);
+      Serial.println("Não toca");
     }
     estadoBuzzer = !estadoBuzzer;
     tempoAnterior = millis();
   }
+  if (nivel == 3) {
+    Serial.println("reseta");
+    resetar();
+  }
+}
+
+void resetar(){
+  delay(5000);
+  noTone(pinoBuzzer);  
+  for( int i = 0; i < 10; i++){
+    digitalWrite(pinoLED1, LOW);
+    digitalWrite(pinoLED2, LOW);
+    digitalWrite(pinoLED3, LOW);
+    delay(150);
+    digitalWrite(pinoLED1, HIGH);
+    digitalWrite(pinoLED2, HIGH);
+    digitalWrite(pinoLED3, HIGH);
+    delay(150);
+  }
+  digitalWrite(pinoLED1, LOW);
+  digitalWrite(pinoLED2, LOW);
+  digitalWrite(pinoLED3, LOW);
+  nivel = 0;
+  tempoAnterior = 0;
+  deltaT = 200;
+  tomBuzzer = 3000;
+  estadoBotao = LOW;
+  estadoBuzzer = HIGH;  
 }
